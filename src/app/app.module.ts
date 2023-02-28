bdc3f44bafe37d9242d255cb43f04a7a57425889
alisonzq <alisonquato@gmail.com>
2023-02-28 00:39:05 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IdeasComponent } from './ideas/ideas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSortModule} from '@angular/material/sort';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    HttpClientModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatSortModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    FormsModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ideas/ideas.component.ts
@@ -0,0 +1,119 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+interface Ideas {
+  setting: string[];
+  story: string[];
+  image: string;
+}
+
+@Component({
+  selector: 'app-ideas',
+  templateUrl: './ideas.component.html',
+  styleUrls: ['./ideas.component.css']
+})
+export class IdeasComponent {
+  prompt: string = '';
+  selectedSetting : string ='';
+  selectedGenre: string = '';
+  selectedTheme: string = '';
+
+  ideas: Ideas = {
+    setting: [],
+    story: [],
+    image: '',
+  };
+
+  themes = [
+    'Climate change',
+    'Deforestation',
+    'Pollution',
+    'Loss of biodiversity',
+    'Water scarcity',
+    'Digital pollution',
+    'Transportation',
+    'Renewable energy',
+    'Fast Fashion',
+  ];
+
+  genres = [
+    'Action',
+    'Adventure',
+    'Fighting',
+    'Platform',
+    'Puzzle',
+    'Racing',
+    'Role-Playing',
+    'Shooter',
+    'Simulation',
+    'Sports',
+    'Strategy',
+  ]
+
+  settings = [
+    'Post-apocalyptic world',
+    'Dystopia',
+    'Sci-fi',
+    'Fantasy',
+    'Battlefield',
+  ]
+
+  constructor(private http: HttpClient) {}
+  
+  generateIdeas(genre: string, theme: string, setting: string) {
+    this.selectedGenre = genre;
+    this.selectedTheme = theme;
+    this.selectedSetting = setting;
+
+    const apiKey = environment.apiKey;
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+    const imageApiUrl = 'https://api.openai.com/v1/images/generations';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', `Bearer ${apiKey}`);
+
+    const storyPrompt = `Write a ${genre.toLowerCase()} video game story set in a ${setting.toLowerCase()} with  this environmental issue as a theme:${theme.toLowerCase()}`
+    this.generateIdea(apiUrl, headers, `${storyPrompt}`, 3, 'story',256);
+
+    // Generate an image based on the prompts
+    this.generateImage(imageApiUrl, headers, `${storyPrompt}`);
+  }
+
+
+
+  generateIdea(apiUrl: string, headers: HttpHeaders, prompt: string, numIdeas: number, element: string, maxTokens: number) {
+    const requestBody = {
+      prompt: prompt,
+      max_tokens: maxTokens,
+      n: numIdeas,
+      stop: ['\n', '}'],
+      temperature: 0.7,
+    };
+    this.http.post<any>(apiUrl, requestBody, { headers }).subscribe(response => {
+      let ideas = response.choices.map((choice: { text: string; }) => choice.text.trim());
+
+
+      // Filter out any names
+      if (element === 'name') {
+        ideas = ideas.filter((idea: string) => {
+          const numWords = idea.split(' ').length;
+          return numWords >= 1 && numWords <= 2;
+        });
+      }
+    
+      this.ideas[element as keyof Ideas] = ideas;
+    });
+  }
+
+  generateImage(apiUrl: string, headers: HttpHeaders, prompt: string) {
+    const requestBody = {
+      "model": "image-alpha-001",
+      "prompt": prompt,
+      "num_images": 1,
+      "size": "512x512",
+      "response_format": "url"
+    };
+    this.http.post<any>(apiUrl, requestBody, { headers }).subscribe(response => {
+      this.ideas.image = response.data[0].url;
+    });
+  }
+}
